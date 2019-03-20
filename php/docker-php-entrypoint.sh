a2dd46cd25db25af83cb657947bf8944e8252a1b
Serhii Hlushko <s.hlushko@gmail.com>
2019-03-20 22:48:05 +0200

    Added support cron task

---
+++ php/docker-php-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+REPOSITORY=${REPOSITORY}
+BRANCH=${BRANCH:-'master'}
+
+if ! [ -d app ]; then
+    echo "Project sources not found. Cloning $BRANCH branch..."
+    git clone -b "$BRANCH" "$REPOSITORY" app
+    cd app
+else
+    echo "Project sources found. Updating $BRANCH branch..."
+    cd app
+    git pull origin "$BRANCH"
+fi
+
+if [ "$1" = "php-fpm" ]; then
+    echo "Installing vendors..."
+    composer install
+
+#    echo "Execute DB migrations..."
+#    app/console doctrine:migrations:migrate -n
+#
+#    echo "Working around assets:"
+#    echo "--> Install..."
+#    app/console assets:install
+#    echo "--> Dump..."
+#    app/console assetic:dump
+#    echo "--> Dump JS Routing..."
+#    app/console fos:js-routing:dump
+
+fi
+
+chown -R www-data:www-data .
+
+exec "$@"
